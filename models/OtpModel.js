722db9176702102437da0355c2661a7721dc3f40
asmdnayeeem <asmdnayeem245@gmail.com>
2024-10-18 00:16:44 +0530

    otp expiry removal

--- models/OtpModel.js
+++ models/OtpModel.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const OTPSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
-    },
-    otp:{
-        type:String,
-        required:true
-    },
-    createdAt:{
-        type:Date,
-        default:Date.now(),
-        expires: 10*60,
-    },
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 module.exports = mongoose.model("OTP", OTPSchema);
- 
